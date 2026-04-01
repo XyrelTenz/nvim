0cb6bd25ca0c0df2d1f247de777ad70e5fd7305a
XyrelTenz <xdemocrito1@gmail.com>
2026-04-01 17:14:48 +0800

    feat(lsp): add volar and vue support with ts_ls plugin
    Add Volar (vue-language-server) to enabled LSP servers and configure
    typescript-language-server (ts_ls) to use @vue/typescript-plugin for Vue
    file support. This enhances Vue development experience with proper LSP
    features.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 local nvlsp = require("nvchad.configs.lspconfig")
+local mason_registry = require("mason-registry")
 
-local servers = { "html", "cssls", "ts_ls", "tailwindcss", "luals", "kotlin_lsp", "jdtls", "gopls", "sqls" }
+local servers = { "html", "cssls", "tailwindcss", "luals", "kotlin_lsp", "jdtls", "gopls", "sqls", "volar" }
 require("telescope").load_extension("projects")
 
 for _, lsp in ipairs(servers) do
@@ -9,6 +10,25 @@ for _, lsp in ipairs(servers) do
 		capabilities = nvlsp.capabilities,
 	})
 end
+
+local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+	.. "/node_modules/@vue/language-server"
+
+vim.lsp.config("ts_ls", {
+	on_attach = nvlsp.on_attach,
+	on_init = nvlsp.on_init,
+	capabilities = nvlsp.capabilities,
+	init_options = {
+		plugins = {
+			{
+				name = "@vue/typescript-plugin",
+				location = vue_language_server_path,
+				languages = { "vue" },
+			},
+		},
+	},
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+})
 
 vim.lsp.config("kotlin_lsp", {
 	-- single_file_support = false,
@@ -44,4 +64,4 @@ vim.lsp.config("gopls", {
 	},
 })
 
-vim.lsp.enable({ "html", "cssls", "ts_ls", "tailwindcss", "kotlin_lsp", "dartls", "jdtls", "gopls", "sqls" })
+vim.lsp.enable({ "html", "cssls", "ts_ls", "tailwindcss", "kotlin_lsp", "dartls", "jdtls", "gopls", "sqls", "volar" })
